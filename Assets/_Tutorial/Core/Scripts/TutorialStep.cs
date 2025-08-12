76fd92c476168c639693a163a98e719e570bd970
Georgiy <egorbogd12@gmail.com>
2025-08-12 15:31:42 +0300

    1

---
+++ Assets/_Tutorial/Core/Scripts/TutorialStep.cs
@@ -0,0 +1,12 @@
+namespace Game.Tutorial
+{
+    public enum TutorialStep
+    {
+        WELCOME = 0,
+        ADD_RESOURCE_TO_CONVEYOR = 1,
+        WAIT_CONVERTATION = 2,
+        TAKE_FROM_CONVEYOR = 3,
+        UPGRADE_CONVEYOR = 4,
+        FINISH = 5
+    }
+}
